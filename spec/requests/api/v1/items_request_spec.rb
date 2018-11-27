02b526a4e6fc16474b4027fe45a0cecfb9d31e1f
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-11-26 18:27:20 -0700

    adds show and update

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,12 +1,38 @@
 require 'rails_helper'
 
 describe "Items API" do
+  
   it "sends list of all items" do
     create_list(:item, 3)
 
     get '/api/v1/items'
 
     expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items.count).to eq(3)
   end
 
+
+  it "can get one item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(item["id"]).to eq(id)
+  end
+
+  it "can create a new item" do
+    item_params = {name: "Saw", description: "Its a scary flick"}
+
+    post "/api/v1/items", params: {item: item_params}
+    item = Item.last
+
+    expect(response).to be_successful
+    expect(item.name).to eq(item_params[:name])
+  end
 end
